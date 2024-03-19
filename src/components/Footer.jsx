59abcbd6264082a3e8023ffd1a730e54d3fe6de9
Sebastian Aguilera <inforaguilera@gmail.com>
2024-03-18 21:32:08 -0300

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,22 +6,15 @@ import "./Footer.css"
 export const Footer = () => {
   return (
     <div className='footer-container'>
-        <div className='footer-form'>
-           <form action="">
-              <input type="text" placeholder='Tu nombre' />
-              <input type="text" placeholder='Email' />
-              <input type="text" placeholder='Teléfono' />
-              <input type="text" placeholder='Mensaje y comentarios' />
-           </form>
-           <Button>Enviar</Button>
-        </div>
         <div className='footer-info'>
             <h2>Reserva con nosotros</h2>
             <span className='span1'><p>A partir de tu contacto, nos comunicaremos contigo para revisar fechas y disponibilidad de horarios.</p></span>
            
+           <div className='centro'>
             <p>Centro Histórico</p>
             <p>Ciudad de México, MX 12345</p>
-                 
+           </div>
+       
             <div className='footer-contacto'>
               <p>+56 987653123</p>
               <p>contacto@email.com</p>  
@@ -29,6 +22,15 @@ export const Footer = () => {
 
             <i>2021. UCamp. Todos los derechos reservados.
              Esta es una aplicación ficticia para fines académicos.</i>
+        </div>
+        <div className='footer-form'>
+           <form action="">
+              <input type="text" placeholder='Tu nombre' />
+              <input type="text" placeholder='Email' />
+              <input type="text" placeholder='Teléfono' />
+              <input type="text" placeholder='Mensaje y comentarios' />
+           </form>
+           <Button className='form-button'>Enviar</Button>
         </div>
     </div>
   )
